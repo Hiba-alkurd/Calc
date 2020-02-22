39051a6dc541542662c88b88ad5cd3ca80d72fe3
King Kong <cuteh@LAPTOP-QM0H46RJ.mynet>
2020-02-22 10:26:25 +0200

    v1

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+
+public class Calculator {
+
+	public int sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
---
+++ src/CalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber() {
+	// Arrange
+	int a = 10;
+	int b = 20;
+	Calculator calc = new Calculator();
+	// Act
+	int result = calc.sum(a, b);
+	// Assert
+	Assert.assertTrue(result > 0);
+	}
+
+}
